432e9a00c2cd64d7cf6dc811fed22fb6138d51e5
hivefans <shidongjie@gmail.com>
2022-07-07 14:26:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: 'hivefans', // 作者
   BIO: '一个普通的IT老兵', // 作者简介
-  LINK: 'https://www.sharec2c.com', // 网站地址
+  LINK: 'https://www.jiagou.ml', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
